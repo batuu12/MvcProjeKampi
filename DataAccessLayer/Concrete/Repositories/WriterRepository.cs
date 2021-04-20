fcdd4b83c53496379d08915bbd2e779fd09f89ec
batuu12 <57187074+batuu12@users.noreply.github.com>
2021-04-20 17:08:40 +0300

    Abstract klasörü eklendi. Interface yapısı eklendi. IRepository eklendi.

---
+++ DataAccessLayer/Concrete/Repositories/WriterRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class WriterRepository : IRepository<Writer>
+    {
+        public void Delete(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
