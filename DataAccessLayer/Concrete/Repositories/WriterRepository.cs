2d31e99cb52c5bd069317bfc9c4518039b04f770
batuu12 <57187074+batuu12@users.noreply.github.com>
2021-04-23 18:17:04 +0300

    BusinessLayer - GenericRepository eklendi..

--- DataAccessLayer/Concrete/Repositories/WriterRepository.cs
+++ DataAccessLayer/Concrete/Repositories/WriterRepository.cs
@@ -2,25 +2,35 @@
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repositories
 {
-    public class WriterRepository : IRepository<Writer>
+    public class WriterRepository : IWriterDal
     {
+        Context c = new Context();
+        DbSet<Category> _object;
+
         public void Delete(Writer p)
         {
             throw new NotImplementedException();
         }
 
-        public List<Writer> GetAll()
+        public void Insert(Writer p)
         {
             throw new NotImplementedException();
         }
 
-        public void Insert(Writer p)
+        public List<Writer> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List(Expression<Func<Writer, bool>> filter)
         {
             throw new NotImplementedException();
         }
