867027ed0db526f492f6b45ff321103362f872d9
batuu12 <57187074+batuu12@users.noreply.github.com>
2021-05-01 20:47:28 +0300

    -Bootstrap kullanımı -Kategorilerin browserda listelenmesi

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -19,14 +19,14 @@ namespace BusinessLayer.Concrete
 
         public void CategoryAddBL(Category p)
         {
-            if (p.CategoryName =="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
-            {
-                // Hata Mesajı..
-            }
-            else
-            {
+            //if (p.CategoryName =="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            //{
+            //    // Hata Mesajı..
+            //}
+            //else
+            //{
                 repo.Insert(p);
-            }
+            //}
         }
     }
 }
